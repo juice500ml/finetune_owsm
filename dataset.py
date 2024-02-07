4f41498c0b8eb75866e7bbd137491487021dae4a
Kwanghee Choi <juice500ml@gmail.com>
2024-02-06 19:22:16 -0500

    fix len 0 case

--- dataset.py
+++ dataset.py
@@ -57,7 +57,9 @@ class FieldworkDataset(Dataset):
                 continue
             if task == "translation" and value["translation_language"] != "en":
                 continue
-            
+            if len(task_id[task]) == 0:
+                continue
+
             speech = path.parent / "audio" / path.stem / key
             text_ctc = value["transcription"]
             text_prev = "<na>"
